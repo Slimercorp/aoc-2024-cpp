d36d7f2664434d27ee2f66b8d5d5801f0e4c9f54
Slimer <slimercorp@gmail.com>
2024-12-06 17:24:29 +0400

    Refactoring

--- day 3/part2.cpp
+++ day 3/part2.cpp
@@ -2,6 +2,52 @@
 #include <iostream>
 #include <regex>
 
+int calculateSum(std::string &line) {
+    const std::regex patternMul(R"(mul\((\d{1,3}),(\d{1,3})\))");
+
+    int sum = 0;
+    for (std::sregex_iterator iter(line.begin(), line.end(), patternMul), end; iter != end; ++iter) {
+        const std::smatch &match = *iter;
+        int firstNumber = std::stoi(match[1].str());
+        int secondNumber = std::stoi(match[2].str());
+
+        sum += firstNumber * secondNumber;
+    }
+    return sum;
+}
+
+void maskRanges(std::string &line) {
+    const std::regex patternDo(R"(do\(\))");
+    const std::regex patternDont(R"(don't\(\))");
+
+    bool mulEnabled = true;
+    std::sregex_iterator end;
+    std::sregex_iterator iterDo(line.begin(), line.end(), patternDo);
+    std::sregex_iterator iterDont(line.begin(), line.end(), patternDont);
+    size_t currentPos = 0;
+    while (iterDont != end || iterDo != end) {
+        if (mulEnabled) {
+            currentPos = iterDont->position();
+            mulEnabled = false;
+            size_t posDo = iterDo->position();
+            while (posDo < currentPos && iterDo != end) {
+                ++iterDo;
+                posDo = iterDo->position();
+            }
+        } else {
+            size_t posDo = iterDo != end ? iterDo->position() : line.size();
+            std::fill(line.begin() + currentPos, line.begin() + posDo, '*');
+            currentPos = posDo;
+            mulEnabled = true;
+            size_t posDont = iterDont->position();
+            while (posDont < currentPos && iterDont != end) {
+                ++iterDont;
+                posDont = iterDont->position();
+            }
+        }
+    }
+}
+
 int main() {
     std::ifstream file("day 3/input.txt");
     if (!file) {
@@ -9,57 +55,13 @@ int main() {
         return 1;
     }
 
-    std::string lineTmp;
-    std::string line;
-    while (std::getline(file, lineTmp)) {
-        line = line.append(lineTmp);
-    }
+    std::ostringstream buffer;
+    buffer << file.rdbuf();
+    std::string line = buffer.str();
 
-    std::regex patternMul(R"(mul\((\d{1,3}),(\d{1,3})\))");
-    std::regex patternDo(R"(do\(\))");
-    std::regex patternDont(R"(don't\(\))");
-    int sum = 0;
-    bool mulEnabled = true;
-    std::sregex_iterator end;
-    std::sregex_iterator iterDo(line.begin(), line.end(), patternDo);
-    std::sregex_iterator iterDont(line.begin(), line.end(), patternDont);
-    int pos = 0;
-    while (true) {
-        if (mulEnabled) {
-            pos = iterDont->position();
-            mulEnabled = false;
-            int posDo = iterDo->position();
-            while (posDo < pos && iterDo != end) {
-                ++iterDo;
-                posDo = iterDo->position();
-            }
-        } else {
-            int posDo = iterDo != end ? iterDo->position() : line.size();
-            for (size_t i = pos; i <= posDo && i < line.size(); ++i) {
-                line[i] = '*';
-            }
-            pos = posDo;
-            mulEnabled = true;
-            int posDont = iterDont->position();
-            while (posDont < pos && iterDont != end) {
-                ++iterDont;
-                posDont = iterDont->position();
-            }
-        }
+    maskRanges(line);
 
-        if (iterDont == end && iterDo == end) {
-            break;
-        }
-    }
+    std::cout <<  calculateSum(line) << std::endl;
 
-    for (std::sregex_iterator iter(line.begin(), line.end(), patternMul); iter != end; ++iter) {
-        const std::smatch &match = *iter;
-        int firstNumber = std::stoi(match[1].str());
-        int secondNumber = std::stoi(match[2].str());
-
-        sum += firstNumber * secondNumber;
-    }
-
-    std::cout << sum << std::endl;
     return 0;
 }
