398b091721efea3967f40e4f9a54c98d7747de06
Slimer <slimercorp@gmail.com>
2024-12-07 16:39:33 +0400

    Day 6

---
+++ day 6/part1.cpp
@@ -0,0 +1,59 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+int main() {
+    std::ifstream file("day 6/input.txt");
+    if (!file) {
+        std::cerr << "Error opening file" << std::endl;
+        return 1;
+    }
+
+    std::vector<std::string> map;
+    std::string line;
+    int x, y;
+    while (getline(file, line)) {
+        map.push_back(line);
+        auto position = line.find('^');
+        if (position != std::string::npos) {
+            x = map.size() - 1;
+            y = position;
+        }
+    }
+
+    int yaw = 0;
+    std::unordered_map<std::string, int> visited;
+    while (x < map.size() && y < map[0].size()) {
+        int nextX = x;
+        int nextY = y;
+        switch (yaw) {
+            case 0:
+                --nextX;
+                break;
+            case 90:
+                ++nextY;
+                break;
+            case 180:
+                ++nextX;
+                break;
+            case 270:
+                --nextY;
+                break;
+        }
+
+        if (nextX >= 0 && nextX < map.size() && nextY >= 0 && nextY < map[0].size() && map[nextX][nextY] == '#') {
+            yaw += 90;
+            yaw = yaw >= 360 ? yaw - 360 : yaw;
+        } else {
+            x = nextX;
+            y = nextY;
+            ++visited[std::to_string(x) + ";" + std::to_string(y)];
+        }
+    }
+
+    std::cout << visited.size() - 1 << std::endl;
+
+
+    return 0;
+}
